f980ed38520245fc0a5a4ce5dc4e7e46d4cf98a6
SahanHeshan <dshsamarathunga@gmail.com>
2023-06-04 13:08:57 +0530

    final v.1.0

--- Js/triangle.js
+++ Js/triangle.js
@@ -1,9 +1,9 @@
 function get1(l1,l2){
-    alert(l2)
+
     let b=parseFloat(l1)
     let h=parseFloat(l2)
     let x = document.getElementById("result");
        
-    return x.innerHTML=(((a*a)+(b*b))**(1/2))
+    return x.innerHTML=((1/2)*h*b)
 
 }
